cd5acc31a7460959b66fede468ce995db80fc68a
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-16 07:29:43 -0700

    Initial setup for spring

---
+++ src/main/java/com/springboot/rupalpaulgspr/introduction/IntroductionApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rupalpaulgspr.introduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionApplication.class, args);
+	}
+
+}
