eae51a08bc1f3116b26bcee1f20ab6f28b013f94
Rupal Paul <rupalpaulgspr@gmail.com>
2024-10-18 13:15:52 -0700

    Homework for introduction

--- src/main/java/com/springboot/rupalpaulgspr/introduction/IntroductionApplication.java
+++ src/main/java/com/springboot/rupalpaulgspr/introduction/IntroductionApplication.java
@@ -1,13 +1,23 @@
 package com.springboot.rupalpaulgspr.introduction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IntroductionApplication {
+public class IntroductionApplication implements CommandLineRunner {
 
+	private final CakeBaker cb;
+	public IntroductionApplication(CakeBaker cb){
+		this.cb=cb;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		cb.bakeCake();
+	}
 }
